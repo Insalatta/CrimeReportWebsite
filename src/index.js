27eab2890aea11f3e1c0876118581c1bf4a3da65
Insalatta <insalattapriya@gmail.com>
2022-03-20 16:19:13 +0530

    Added some gestures to header

--- src/index.js
+++ src/index.js
@@ -7,6 +7,52 @@ import Crime from "./Pages/Crime";
 import About from "./Pages/About";
 import Search from "./Pages/Search";
 class Header extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      homeActive:false,
+      reportActive:false,
+      searchActive:false,
+      aboutActive:false,
+    }
+    this.handleHome = this.handleHome.bind(this);
+    this.handleReport = this.handleReport.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handleAbout = this.handleAbout.bind(this);
+   
+  }
+  handleHome =() =>{
+    this.setState({
+      homeActive:true,
+      reportActive:false,
+      searchActive:false,
+      aboutActive:false,
+    });
+  }
+  handleReport =() =>{
+    this.setState({
+      homeActive:false,
+      reportActive:true,
+      searchActive:false,
+      aboutActive:false,
+    });
+  }
+  handleSearch =() =>{
+    this.setState({
+      homeActive:false,
+      reportActive:false,
+      searchActive:true,
+      aboutActive:false,
+    });
+  }
+  handleAbout =() =>{
+    this.setState({
+      homeActive:false,
+      reportActive:false,
+      searchActive:false,
+      aboutActive:true,
+    });
+  }
   render(){
     return(
       <div className="wrapper">  
@@ -16,10 +62,10 @@ class Header extends React.Component{
               <p className="head">Crime Report</p>
               <p className="sub">Report crime before it cast another life.</p>
             </div>
-            <Link to="/" className="elements">Home</Link>
-            <Link to="/crime" className="elements">Report</Link>
-            <Link to="/search" className="elements">Search</Link>
-            <Link to="/about" className="elements">About</Link>
+            <Link to="/" className={"elements "+(this.state.homeActive?"active":"")} onClick={this.handleHome}>Home</Link>
+            <Link to="/crime" className={"elements "+(this.state.reportActive?"active":"")} onClick={this.handleReport}>Report</Link>
+            <Link to="/search" className={"elements "+(this.state.searchActive?"active":"")} onClick={this.handleSearch}>Search</Link>
+            <Link to="/about" className={"elements "+(this.state.aboutActive?"active":"")} onClick={this.handleAbout}>About</Link>
           </div>
         </div>
       </div>
