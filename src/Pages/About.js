4206d895d6c5ebc4e5ac2f6fb23947eb23e7dcd6
Insalatta <insalattapriya@gmail.com>
2022-03-20 16:02:45 +0530

    Added about page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -6,7 +6,26 @@ class About extends React.Component{
         return(
             <div className="homeWrapper">
                 <img src={p4} className="image" />
-                <p className="homeHeader">About Page</p>
+                <div className="aboutContent">
+                    <p>This website helps the society to 
+                        report crime and let people know 
+                        about the crimes happened in the 
+                        past and present with the date and 
+                        place specified in it. You can search
+                        specific place where you want to know 
+                        the crime had happened. Let's make this 
+                        society a better place to live!
+                        <br></br><br></br>
+                        This Webpage is developed by the students at 
+                        Government College of Engineering.
+                        <br></br>
+                        Insalatta Priyadharshini S <br></br>
+                        Nagalakshmi <br></br>
+                        Sornalatha <br></br>
+                        Kavitha S<br></br>
+
+                    </p>
+                </div>
             </div>
         )
     }
